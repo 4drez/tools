4547016b2daf2d62f7e1edd11ec646f3969c31a9
4drez <124504692+4drez@users.noreply.github.com>
2023-12-20 02:08:31 +0100

    hostdiscovery.sh

--- hostdiscovery.sh
+++ hostdiscovery.sh
@@ -6,6 +6,6 @@
 #!/bin/bash
 
 for i in $(seq 1 254); do
-        timeout 1 bash -c "ping -c 1 192.168.100.i$" &>/dev/null && echo "[+] Host 192.168.100.i$ - Active" &
+        timeout 1 bash -c "ping -c 1 192.168.100.$i" &>/dev/null && echo "[+] Host 192.168.100.$i - ACTIVE" & 
 done; wait
 
